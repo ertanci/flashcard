0f21ad5051e96478ebf02d9b9517c000e666c438
ertanc <ertanciftlik02@gmail.com>
2026-05-08 01:23:23 +0300

    fix: update base path for GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import {defineConfig, loadEnv} from 'vite';
 export default defineConfig(({mode}) => {
   const env = loadEnv(mode, '.', '');
   return {
-    base: '/ai-flashcard-pro/', // <-- GITHUB PAGES İÇİN EKLENEN SATIR BURASI
+    base: '/flashcard/', // <-- GITHUB PAGES İÇİN EKLENEN SATIR BURASI
     plugins: [react(), tailwindcss()],
     define: {
       'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY),
